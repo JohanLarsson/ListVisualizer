d312bc02928b1b34922cdf46ba5a3ef52020234d
Johan Larsson <johan_gavle1@hotmail.com>
2014-01-20 01:48:35 +0100

    pretty broken, still a push :D

---
+++ EnumerableVisualizer/Visualizer.cs
@@ -0,0 +1,18 @@
+﻿namespace EnumerableVisualizer
+{
+    using System.Collections.Generic;
+    using System.Windows;
+    using Microsoft.VisualStudio.DebuggerVisualizers;
+
+    public class Visualizer : DialogDebuggerVisualizer
+    {
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            var window = new Window
+            {
+                Content = new View((IEnumerable<object>) objectProvider.GetObject())
+            };
+            window.ShowDialog();
+        }
+    }
+}
---
+++ EnumerableVisualizer/Vm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableVisualizer
+{
+    using System.Windows.Data;
+
+    public class Vm
+    {
+        public Vm(IEnumerable<object> list )
+        {
+            CollectionView= new ListCollectionView(list.ToList());
+        }
+        public ListCollectionView CollectionView { get; private set; }
+    }
+}
